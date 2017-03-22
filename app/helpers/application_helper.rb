6dddbf0fc23f677b3239b4a858af7787424c2c3d
H2V <h2v9696@gmail>
2017-03-22 18:54:48 +0700

    Add static page
    Add layout
    
    Fix
    
    Fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def full_title page_title = ""
+    base_title = "Ruby on Rails Tutorial"
+    page_title.empty? ? base_title : "#{page_title} | #{base_title}"
+  end
 end
