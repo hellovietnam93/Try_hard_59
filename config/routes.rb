53aa8043c12db5d01ebee1ffae780bf6522ec216
s2luckyboy <nguyenvanduong.vn.k59@gmail.com>
2017-03-23 19:53:16 +0700

    signup

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get "help", to: "static_pages#help"
   get "about", to: "static_pages#about"
   get "contact", to: "static_pages#contact"
-  get  "/signup",  to: "users#new"
+  get "/signup", to: "users#new"
   root "static_pages#home"
+  post "/signup", to: "users#create"
+  resources :users
 end
