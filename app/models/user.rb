f517167ab83660770e63291e90fe456686ab02cf
H2V <h2v9696@gmail.com>
2017-03-23 19:13:30 +0700

    Sign Up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   before_save {email.downcase!}
 
-  validates :name, presence: true, length: {maximum: 50}
+  validates :name, presence: true, length: {minimum: 6, maximum: 50}
 
   VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: 255},
