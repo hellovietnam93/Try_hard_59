cda3ddda18648fc5d40dcd34f55574a2bb5311e6
TardigradeS <dothisnows@gmail.com>
2017-03-24 23:53:38 +0700

    Add sign up pages + show users pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get "/help", to: "static_pages#help"
   get "/contact", to: "static_pages#contact"
   get "/about", to: "static_pages#about"
-  root "static_pages#home"
   get "/signup", to: "users#new"
+  root "static_pages#home"
+
+  resources :users
 end
