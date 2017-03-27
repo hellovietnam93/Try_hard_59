aa87d6d391ae4ad46c8ce0c421fe4e1aee9a3fe4
manjusaka <sakurano96@gmail.com>
2017-03-27 23:50:04 +0700

    advanced_login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170326172107) do
+ActiveRecord::Schema.define(version: 20170327124310) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20170326172107) do
     t.string   "telephone_number"
     t.datetime "birthday"
     t.string   "password_digest"
+    t.string   "remember_digest"
   end
 
 end
