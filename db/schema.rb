002227fb98e750f5aee20adbbdff2095329cc334
TardigradeS <dothisnows@gmail.com>
2017-04-16 18:08:06 +0700

    Use remember token to login

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170323113836) do
+ActiveRecord::Schema.define(version: 20170416104503) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "remember_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
