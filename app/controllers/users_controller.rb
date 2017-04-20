4fbd3087937ec6cee0cf6399e713ea7308bc6ba9
bloodarchduke <bloodarchduke@gmail.com>
2017-04-20 18:53:29 +0700

    updating_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,50 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, except: [:new, :create, :show ]
+  before_action :verify_admin, only: :destroy
+  before_action :find_user, except: [:index, :new, :create]
+  before_action :correct_user, only: [:edit, :update]
+
+  def index
+    @users = User.paginate page: params[:page], per_page: 15
+  end
 
   def new
     @user = User.new
-  end
-
-  def show
-    @user = User.find params[:id]
   end
 
   def create
     @user = User.new user_params
     if @user.save
       log_in @user
-      flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = t ".welcome"
       redirect_to @user
     else
-      render "new"
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = t ".update"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      flash[:success] = t ".destroy"
+      redirect_to users_url
+    else
+      flash[:danger] = t ".cannot_destroy"
+      redirect_to @user
     end
   end
 
@@ -26,4 +55,27 @@ class UsersController < ApplicationController
       :password_confirmation
   end
 
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = t ".login"
+      redirect_to login_url
+    end
+  end
+
+  def find_user
+    @user = User.find_by id: params[:id]
+    unless @user
+      flash[:danger] = t ".find"
+      redirect_to root_url
+    end
+  end
+
+  def correct_user
+    redirect_to root_url unless current_user.is_user? @user
+  end
+
+  def verify_admin
+    redirect_to root_url unless current_user.admin?
+  end
 end
