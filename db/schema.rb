7763ce6788b98a93cab4a702a6f1de43ce9f9a47
Hoang Anh Tu <hoangtuyb96@gmail.com>
2017-04-23 21:25:43 +0700

    Account_activation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170417163155) do
+ActiveRecord::Schema.define(version: 20170420120554) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -18,10 +18,13 @@ ActiveRecord::Schema.define(version: 20170417163155) do
     t.string   "phone"
     t.date     "birthday"
     t.string   "password_digest"
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
     t.string   "remember_digest"
-    t.boolean  "admin",           default: false
+    t.boolean  "admin",             default: false
+    t.string   "activation_digest"
+    t.boolean  "activated",         default: false
+    t.datetime "activated_at"
   end
 
 end
