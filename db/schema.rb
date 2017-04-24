84b8ede24cf1c40827d24f826ff25be9f4d18446
TUNG LAM <coldwind1911@gmail.com>
2017-04-24 18:14:41 +0700

    Remember + Update user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170323041818) do
+ActiveRecord::Schema.define(version: 20170408031118) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20170323041818) do
     t.date     "birthday"
     t.string   "phone_number"
     t.string   "password_digest"
+    t.string   "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
