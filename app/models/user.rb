af2f654ab0551301bbec1a53389c77660ed40845
nguyenvanduong20140887 <nguyenvanduong.vn.k59@gmail.com>
2017-05-23 12:35:25 +0700

    chapter13+14

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,16 @@ class User < ApplicationRecord
 
   has_secure_password
 
+  has_many :microposts, dependent: :destroy
+
+  has_many :active_relationships, class_name: "Relationship",
+    foreign_key: "follower_id", dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+
+  has_many :passive_relationships, class_name:  "Relationship",
+    foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :passive_relationships, source: :follower
+
   class << self
     def digest string
       cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
@@ -65,6 +75,27 @@ class User < ApplicationRecord
 
   def password_reset_expired?
     reset_sent_at < Settings.time_reset.hours.ago
+  end
+
+  def feed
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE  follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
+  end
+
+  def follow other_user
+    following << other_user
+  end
+
+  # Unfollows a user.
+  def unfollow other_user
+    following.delete(other_user)
+  end
+
+  # Returns true if the current user is following the other user.
+  def following? other_user
+    following.include?(other_user)
   end
 
   private
--- config/application.rb
+++ config/application.rb
@@ -7,5 +7,6 @@ Bundler.require(*Rails.groups)
 module TryHard59
   class Application < Rails::Application
     config.i18n.default_locale = :en
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
